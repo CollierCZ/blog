2f5571956e378e2ef7ddf2b0a67dd837111ad57b
Aaron Collier <aaronc@kentico.com>
2018-09-19 13:27:38 +0200

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,70 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+import { css } from "react-emotion"
+import { rhythm } from "../utils/typography"
+import Layout from "../components/layout"
+
+export default ({ data }) => {
+  console.log(data)
+  return (
+    <Layout>
+      <div>
+        <h1
+          className={css`
+            display: inline-block;
+            border-bottom: 1px solid;
+          `}
+        >
+          {data.site.siteMetadata.title}
+        </h1>
+        {data.allKenticoCloudItemArticle.edges.map(({ node }) => (
+          <div key={node.fields.slug}>
+          <Link
+            to={`../article/${node.fields.slug}`}
+            className={css`
+              text-decoration: none;
+              color: inherit;
+            `}
+          >
+            <h3
+              className={css`
+                margin-bottom: ${rhythm(1 / 4)};
+              `}
+            >
+              {node.title.value}{" "}
+              <span
+                className={css`
+                  color: #bbb;
+                `}
+              >
+              </span>
+            </h3>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query SiteTitleQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }    
+    allKenticoCloudItemArticle {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          title {
+            value
+          }
+        }
+      }
+    }
+  }
+`
