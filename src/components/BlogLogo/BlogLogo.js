c90516276b1805c8709c0b130b17b68a3cfa2a6e
Aaron Collier <aaronc@kentico.com>
2019-03-01 15:05:58 +0100

    Update React and use useStaticQuery

--- src/components/BlogLogo/BlogLogo.js
+++ src/components/BlogLogo/BlogLogo.js
@@ -1,35 +1,30 @@
 import React from "react";
-import { Link, graphql, StaticQuery } from "gatsby";
+import { Link, graphql, useStaticQuery } from "gatsby";
 import "./BlogLogo.css";
 
-const BlogLogo = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        kenticoCloudItemHome{
-          elements {
-            title {
-              value
-            }
-            blog_logo {
-              assets {
-                url
-              }
+const BlogLogo = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      kenticoCloudItemHome{
+        elements {
+          title {
+            value
+          }
+          blog_logo {
+            assets {
+              url
             }
           }
         }
       }
-    `}
-    render = {data => {
-      return (
-        <Link className="blog-logo" to={"/"}>
-          {/* style={{ boxShadow: "none" }}> */}
-          <img src={data.kenticoCloudItemHome.elements.blog_logo.assets[0].url} alt={data.kenticoCloudItemHome.elements.title.value} />
-        </Link>
-      )
     }
-  }
-  />
-)
+  `)
+  return (
+    <Link className="blog-logo" to={"/"}>
+      {/* style={{ boxShadow: "none" }}> */}
+      <img src={data.kenticoCloudItemHome.elements.blog_logo.assets[0].url} alt={data.kenticoCloudItemHome.elements.title.value} />
+    </Link>
+  )    
+}
 
 export default BlogLogo;
