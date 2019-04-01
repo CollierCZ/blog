f6bba890ec67b4c4f2b2a900efa8947bbc5390ce
Aaron Collier <aaronc@kentico.com>
2019-04-01 10:44:18 +0200

    Add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,31 @@
+import React from "react"
+import ArticleFormatting from "../components/ArticleFormatting"
+import ArticleHeader from "../components/ArticleHeader"
+import BlogLogo from "../components/BlogLogo"
+import Footer from "../components/Footer"
+import Layout from "../layouts/SiteWrapper/SiteWrapper"
+import MainHeader from "../components/MainHeader"
+import MainNav from "../components/MainNav"
+import SEO from "../components/SEO"
+
+export default ({ data }) => {
+  return (
+    <>
+      <SEO />
+      <Layout>
+        <MainHeader headStyle="small">
+          <MainNav>
+            <BlogLogo />
+          </MainNav>
+        </MainHeader>
+        <ArticleFormatting>
+          <ArticleHeader>
+            <h1>404</h1>
+            <p>I'm afraid I can't find what you're looking for.</p>
+          </ArticleHeader>
+        </ArticleFormatting>
+        <Footer author="Aaron Collier" />
+      </Layout>
+    </>
+)
+}
