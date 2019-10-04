1cc0a99953afc4304d64809653480d0231738c43
Aaron Collier <aaron@collier.cz>
2019-10-04 18:03:46 +0200

    Fix adaptive width

--- src/components/RichText/InlineImage.js
+++ src/components/RichText/InlineImage.js
@@ -1,18 +1,22 @@
+import { css } from '@emotion/core'
 import PropTypes from 'prop-types'
 import React from 'react'
 
 const InlineImage = ({ description, url }) => {
   return (
     <picture>
-    <source srcSet={url+'?w=1200&fit=scale'}
-            media="(min-width: 1500px)" />
-            <source srcSet={url+'?w=1200&fit=scale'}
-                    media="(min-width: 720px)" />
-        <source srcSet={url+'?w=500&fit=scale'}
-                media="(min-width: 400px)" />
-        <img
-            src={url+'?w=300&fit=scale'}
-            alt={description}
+      <source srcSet={url+'?w=1200&fit=scale'}
+        media="(min-width: 1500px)" />
+      <source srcSet={url+'?w=1100&fit=scale'}
+        media="(min-width: 720px)" />
+      <source srcSet={url+'?w=500&fit=scale'}
+        media="(min-width: 400px)" />
+      <img 
+        css={css`
+          max-width: 100%;
+        `}
+        src={url+'?w=300&fit=scale'}
+        alt={description}
         />
     </picture>
   )
