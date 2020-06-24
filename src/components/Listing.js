affcada83598a7abf25dfbd814760c6cd3fa6d5c
Aaron Collier <aaron@collier.cz>
2020-06-24 09:39:42 +0200

    fix: stop propagation of links in tiles

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -1,8 +1,8 @@
-import React from "react"
-import ArticleListing from "../components/ArticleListing"
-import BlogLogo from "../components/BlogLogo"
+import React from "react";
+import ArticleListing from "../components/ArticleListing";
+import BlogLogo from "../components/BlogLogo";
 import Footer from "../components/Footer";
-import Layout from "../layouts/SiteWrapper/SiteWrapper"
+import Layout from "../layouts/SiteWrapper/SiteWrapper";
 import MainHeader from "../components/MainHeader";
 import MainNav from "../components/MainNav";
 import PageTitle from "../components/PageTitle";
@@ -12,19 +12,19 @@ import SEO from "../components/SEO";
 import SocialMediaIcons from "../components/SocialMediaIcons";
 import "../layouts/index.css";
 
-const Listing = ({context, data, headStyle}) => {
-  const {
-    categoryName,
-    group,
-    index,
-    pageCount,
-    tag
-  } = context;
+const Listing = ({ context, data, headStyle }) => {
+  const { categoryName, group, index, pageCount, tag } = context;
   const nodes = data.articles.edges;
   const socialUrls = data.config.elements.socialmedia.value.split(",");
   const additionalInfo = tag ? tag : categoryName ? categoryName : null;
-  const additionalDescription = tag ? `Articles tagged with ${tag}`: data.header ? data.header.elements.metadata__description.value : null;
-  const cover = data.header ? data.header.elements.banner_image.value[0].url : null;
+  const additionalDescription = tag
+    ? `Articles tagged with ${tag}`
+    : data.header
+    ? data.header.elements.metadata__description.value
+    : null;
+  const cover = data.header
+    ? data.header.elements.banner_image.value[0].url
+    : null;
 
   return (
     <>
@@ -33,27 +33,26 @@ const Listing = ({context, data, headStyle}) => {
         <MainHeader cover={cover} headStyle={headStyle}>
           <MainNav>
             <BlogLogo />
-            <SocialMediaIcons
-              urls={socialUrls}
-            />
+            <SocialMediaIcons urls={socialUrls} />
           </MainNav>
-            <div className="main-header-content">
-              <PageTitle text={additionalInfo} />
-              <PageDescription text={additionalDescription} />
-            </div>
+          <div className="main-header-content">
+            <PageTitle text={additionalInfo} />
+            <PageDescription text={additionalDescription} />
+          </div>
         </MainHeader>
         <div>
-        <PaginatedContent
-              page={index}
-              pageCount={pageCount}
-            >
-          <ArticleListing articleEdges={nodes} index={index} count={group.length} />
+          <PaginatedContent page={index} pageCount={pageCount}>
+            <ArticleListing
+              articleEdges={nodes}
+              index={index}
+              count={group.length}
+            />
           </PaginatedContent>
         </div>
         <Footer author="Aaron Collier" />
       </Layout>
     </>
-  )
-}
+  );
+};
 
 export default Listing;
--- src/components/RichText/RichText.js
+++ src/components/RichText/RichText.js
@@ -9,16 +9,17 @@ import {
   List,
   ListItem,
   Text,
-  TextLink
+  TextLink,
 } from "@kiwicom/orbit-components";
 
-const RichText = ({ content, images, links, linkedItems }) => {
+const RichText = ({ content, images, links, linkedItems, stopPropagation }) => {
   if (!content || !content.length) {
     return null;
   }
   // Parse HTML as React components, replacing any content items.
   const children = parseHTML(content, {
-    replace: domNode => replaceNode(domNode, images, links, linkedItems)
+    replace: (domNode) =>
+      replaceNode(domNode, images, links, linkedItems, stopPropagation),
   });
 
   return <>{children}</>;
@@ -43,23 +44,24 @@ const replaceLists = {
     if (name === "a") {
       return replaceLinks(attribs, children);
     }
-  }
+  },
 };
 
-const replaceLinks = (attribs, children, links) => {
+const replaceLinks = (attribs, children, links, stopPropagation) => {
   // Replace internal links
   if (attribs["data-item-id"]) {
     const id = attribs["data-item-id"] || null;
-    const link = links.find(link => link.link_id === id);
+    const link = links.find((link) => link.link_id === id);
     const url = `/articles/${link.url_slug}`;
     return (
       <TextLink
         href={url}
-        onClick={event => {
+        onClick={(event) => {
           event.preventDefault();
           navigate(url);
         }}
         key={url}
+        stopPropagation={stopPropagation}
       >
         {children[0].data}
       </TextLink>
@@ -69,7 +71,7 @@ const replaceLinks = (attribs, children, links) => {
   //Replace external links
   const url = attribs["href"] || null;
   return (
-    <TextLink href={url} key={url}>
+    <TextLink href={url} key={url} stopPropagation={stopPropagation}>
       {children[0].data}
     </TextLink>
   );
@@ -79,19 +81,20 @@ RichText.propTypes = {
   content: PropTypes.string.isRequired,
   images: PropTypes.arrayOf(PropTypes.object),
   links: PropTypes.arrayOf(PropTypes.object),
-  linkedItems: PropTypes.arrayOf(PropTypes.object)
+  linkedItems: PropTypes.arrayOf(PropTypes.object),
+  stopPropagation: PropTypes.bool,
 };
 
 export default RichText;
 
 /** Replace HTML DOM node with React component. */
-function replaceNode(domNode, images, links, linkedItems) {
+function replaceNode(domNode, images, links, linkedItems, stopPropagation) {
   const { attribs, children, name } = domNode;
   let content = [];
   // Replace inline assets.
   if (name === "figure") {
     const id = attribs["data-asset-id"] || null;
-    const image = images.find(image => image.image_id === id);
+    const image = images.find((image) => image.image_id === id);
     return <InlineImage description={image.description} url={image.url} />;
   }
 
@@ -99,16 +102,18 @@ function replaceNode(domNode, images, links, linkedItems) {
   if (attribs && attribs["data-type"] === "item") {
     const codename = attribs["data-codename"] || null;
     const linkedItem = linkedItems.find(
-      item => item.system.codename === codename
+      (item) => item.system.codename === codename
     );
     return <LinkedItem linkedItem={linkedItem} />;
   }
 
   // Replace paragraphs.
   if (domNode.name === "p") {
-    children.forEach(node => {
+    children.forEach((node) => {
       if (node.name === "a") {
-        content.push(replaceLinks(node.attribs, node.children, links));
+        content.push(
+          replaceLinks(node.attribs, node.children, links, stopPropagation)
+        );
       }
       if (node.type === "text") {
         content.push(node.data);
